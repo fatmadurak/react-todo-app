bc943eb96e6b2bad9a2e03cfff481ff1b0bbde0b
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-10 14:09:03 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import Content from "./components/Content";
+import Header from "./components/Header";
 import "./index.css"
 
 function App() {
   return (
-    <div className="App">
+    <section class="todoapp">
 
-    </div>
+    <Header/>
+    <Content/>
+
+
+
+    </section>
   );
 }
 
