ff24c0ca59df550c0475256b1b025d6db120ad34
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-11 11:46:00 +0300

    commited

--- src/components/Header/NewTodoForm.js
+++ src/components/Header/NewTodoForm.js
@@ -1,30 +1,23 @@
 import React from 'react'
-import {Field, Form, Formik} from "formik"
-
-
+import {Form,Formik} from "formik";
 
 function NewTodoForm() {
   return (
-   <Formik>
-
-   initialValues={{
-    text:"",
-
-  }}
-
-   onSubmit={async(values)=>{
-    
-    await new Promise((r)=>setTimeout(r,500));
-    alert(JSON.stringify(values,null,2));
-
-    }}
-
-
-  <Form>
-    <Field className="new-todo" placeholder="What needs to be done?" autoFocus id="text" name="text" />
-  </Form>
-
-   </Formik>
+    <Formik
+      initialValues={{
+        firstName: '',
+      
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+       <input className='new-todo' placeholder='What needs to be done?' autoFocus/>
+       
+      </Form>
+    </Formik>
   )
 }
 
